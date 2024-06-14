a98478bf7824ba0874a8755fe9ee21776033a393
Sadkhan Mulla <164398962+SadkhanMulla@users.noreply.github.com>
2024-06-14 23:05:21 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { 3, 2, 6, 1, 8, 5, 0 };
+        bubbleSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
